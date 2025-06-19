62b551e6d9bf793c91c39b93f692c4405b2a93cd
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2025-06-19 17:25:35 +0500

    Error Page developed

--- client/src/components/styled-components/Buttons.jsx
+++ client/src/components/styled-components/Buttons.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./Style.css";
+import { useNavigate } from "react-router-dom";
 export const Buttons = ({ title }) => {
   return <button className="btn">{title}</button>;
 };
 
-export const Buttonss = ({ title }) => {
-  return <button className="btn2">{title}</button>;
+export const Buttonss = ({ title, link }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/home");
+  };
+  return (
+    <button onClick={handleClick} className="btn2">
+      {title}
+    </button>
+  );
 };
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,40 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React from "react";
-
+import error from "../assets/404.png";
+import { Buttonss } from "../components/styled-components/Buttons";
 const Error = () => {
   return (
     <Stack
-      height={"80vh"}
+      padding={{ xs: "4rem 0rem" }}
+      height={{ xs: "100%", sm: "100vh" }}
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
-    ></Stack>
+      className="error"
+      spacing={1}
+    >
+      <img src={error} alt="" />
+      <Typography
+        variant="h3"
+        textAlign={"center"}
+        fontSize={{ xs: "1.6rem", sm: "2rem", md: "2.2rem" }}
+        fontWeight={"bold"}
+      >
+        That Page Cant Be Found
+      </Typography>
+      <Typography
+        fontSize={"16px"}
+        width={{ xs: "90%", sm: "70%", md: "40%" }}
+        textAlign={"center"}
+        color="#6B7280"
+      >
+        It looks like nothing was found at this location. Maybe try to search
+        for what you are looking for?
+      </Typography>
+      <Stack width={"200px"} marginTop={"1rem!important"}>
+        <Buttonss title={"Go To Homepage"} />
+      </Stack>
+    </Stack>
   );
 };
 
